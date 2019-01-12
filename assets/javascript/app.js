8dd82e989cb61e619e85575a602c599bb5095c5d
tommaroney <jasonbeisigel@gmail.com>
2019-01-11 22:52:35 -0500

    layout looks about right

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+let screenDrop = [ "cheers.jpg", "theOffice.jpg", "friends.jpg", "fullHouse.jpg",
+                 "seinfeld.jpg", ]
+
+$(document).ready(function() {
+    $("#screen").attr("src", "assets/images/fullHouse.jpg");
+});
